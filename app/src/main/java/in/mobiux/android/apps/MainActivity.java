6be4a3a54079bf678920c0c3318d313708ef59ff
Sujeet Kumar <sujeet@mobiux.in>
2022-07-13 14:01:40 +0530

    module imported

--- app/src/main/java/in/mobiux/android/apps/MainActivity.java
+++ app/src/main/java/in/mobiux/android/apps/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import in.mobiux.android.commonlibs.utils.AppLogger;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppLogger.getInstance();
     }
 }
